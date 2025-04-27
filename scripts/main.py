771683e4ab7dfef25f99b31912fb1a15c31c91b0
JakubMurin <murin43@uniba.sk>
2025-04-28 00:00:07 +0200

    Use case to sequence diagram generating

--- scripts/main.py
+++ scripts/main.py
@@ -36,19 +36,23 @@ def calculate_price(prompt_tokens: int, completion_tokens: int) -> float:
     
     return prompt_tokens * input_price + completion_tokens * output_price
 
-def merge_metadata(uc_data: dict, seq_data: dict) -> dict:
+def merge_metadata(uc_data: dict, seq_data: dict, name: str="") -> dict:
     """
     Returns final representation of dataset entry
     """
     uc_data = {"uc_" + key: value for key, value in uc_data.items()}
     seq_data = {"seq_" + key: value for key, value in seq_data.items()}
     return {
-        "name": "",
-        "domain": "",
+        "name": name,
         "origin": "semi-synthetic",
         **uc_data,
         **seq_data,
     }
+    
+def save_error(save_path: str, given_data: dict, temp: float, top_p: float, type_e: str, content: str):
+    with open(save_path, "a", encoding="utf-8") as wrong_file:
+        data = {"type": type_e, "data": given_data, "temperature": temp, "top_p": top_p, "content": content}
+        print(json.dumps(data), file=wrong_file)
             
 
 ############### SEQUENCE -> USECASE ###############
@@ -85,22 +89,23 @@ def seq_uc():
 
     with open(DTD_SCHEME_PATH, "r") as file:
         dtd = file.read()
+        
+    price = 0
+    start = time.perf_counter()
+    counter = 0
 
     with open(seq_data_path, "r", encoding="utf-8") as seq_entry:
-        price = 0
         for seq in seq_entry:
             seq_data = json.loads(seq)
+            print(seq_data["file"])
+            
             if seq_data["origin"] == Origin.CHAT_GPT:
                 continue
-            
-            start = time.perf_counter()
-            
+                        
             for value, is_temp in product(*params.values()):
+                counter += 1
                 
-                if is_temp:
-                    temp, top_p = round(value + 0.4, 1), 1
-                else:
-                    temp, top_p = 1, value
+                temp, top_p = (round(value + 0.4, 1), 1) if is_temp else (1, value)
                 
                 seq_path = os.path.join(RESULT_DATA_PATH, "seq", seq_data["file"])
                 with open(seq_path, "r", encoding="utf-8") as file:
@@ -118,25 +123,27 @@ def seq_uc():
                 
                 price += calculate_price(result["usage"]["prompt_tokens"], result["usage"]["completion_tokens"])
                 
-                # domain = re.search(r"\{\{(.*)\}\}", content).group(1)
                 usecase = content.replace("```xml", "").replace("```", "").replace('<!DOCTYPE useCase SYSTEM "usecase.dtd">', "")
                             
                 uc_data = xml_files.process_usecase(usecase, Origin.CHAT_GPT, "", context.ai_model, temp, top_p)
                 
                 if uc_data is None:
-                    with open(os.path.join(RESULT_DATA_PATH, "wrong_uc.jsonl"), "a", encoding="utf-8") as wrong_file:
-                        data = {"seq": seq_data, "temperature": temp, "top_p": top_p, "content": content}
-                        print(json.dumps(data), file=wrong_file)
+                    # with open(os.path.join(RESULT_DATA_PATH, "wrong_uc.jsonl"), "a", encoding="utf-8") as wrong_file:
+                    #     data = {"seq": seq_data, "temperature": temp, "top_p": top_p, "content": content}
+                    #     print(json.dumps(data), file=wrong_file)
+                    save_error(os.path.join(RESULT_DATA_PATH, "wrong_uc.jsonl"), seq_data, temp, top_p, "Wrong format", content)
                     continue
                     
+                name = seq_data["title"]
                 print(json.dumps(uc_data), file=usecase_file)
-                print(json.dumps(merge_metadata(uc_data, seq_data)), file=output_file)
+                print(json.dumps(merge_metadata(uc_data, seq_data, name=name)), file=output_file)
                 
-            print(time.perf_counter() - start, f"price: {price}$")
-            break
-        
+                        
     output_file.close()
     usecase_file.close()
+    
+    total = time.perf_counter() - start
+    print(f"Total time: {total}", f"Average time: {total / counter}", f"price: {price}$")
 
 ############### USECASE -> SEQUENCE ###############
 def uc_seq():
@@ -145,28 +152,31 @@ def uc_seq():
     output_file = open(os.path.join(RESULT_DATA_PATH, "all.jsonl"), "a", encoding="utf-8")
     seq_file = open(os.path.join(RESULT_DATA_PATH, "seq.jsonl"), "a", encoding="utf-8")
 
+    price = 0
+    start = time.perf_counter()
+    counter = 0
+
     with open(usecase_data_path, "r", encoding="utf-8") as usecases_data_file:
-        price = 0
-        for uc in usecases_data_file:
+        for uc in usecases_data_file:            
             uc_data = json.loads(uc)
+            print(uc_data["file"])
+            
             if uc_data["origin"] == Origin.CHAT_GPT:
                 continue
-            
-            start = time.perf_counter()
-            
+                        
             for value, is_temp in product(*params.values()):
+                counter += 1
                 
-                if is_temp:
-                    temp, top_p = round(value + 0.4, 1), 1
-                else:
-                    temp, top_p = 1, value
+                temp, top_p = (round(value + 0.4, 1), 1) if is_temp else (1, value)
                 
-                uc_path = os.path.join(RESULT_DATA_PATH, "uc", uc_data["file"])
+                uc_path = os.path.join(RESULT_DATA_PATH, uc_data["file"])
                 with open(uc_path, "r", encoding="utf-8") as file:
                     useCase = file.read()
 
-                promt_ctx = [{"role": "user", "content": "Based on the following use case, create a sequence diagram in plantuml without any comments."},
-                            {"role": "user", "content": "In sequence diagram if name of element is multiple words, put it in quotation marks and give diagram title"}]
+                promt_ctx = [
+                    {"role": "user", "content": "Based on the following use case, create a sequence diagram in plantuml without any comments."},
+                    {"role": "user", "content": "In sequence diagram if name of element is multiple words, put it in quotation marks and give diagram title"},
+                    ]
                 query = f"Here is use case {useCase}"
                 
                 result = context.execute_query(query, promt_ctx, temp, top_p)[0]
@@ -174,26 +184,33 @@ def uc_seq():
                 
                 price += calculate_price(result["usage"]["prompt_tokens"], result["usage"]["completion_tokens"])
                 
-                # domain = re.search(r"\{\{(.*)\}\}", content).group(1)
                 seq_diag = re.search(r"(@startuml.*@enduml)", content, re.DOTALL)
                 
                 if seq_diag is None:
-                    with open(os.path.join(RESULT_DATA_PATH, "wrong.jsonl"), "a", encoding="utf-8") as wrong_file:
-                        data = {"uc": uc_data, "temperature": temp, "top_p": top_p, "content": content}
-                        print(json.dumps(data), file=wrong_file)
+                    # with open(os.path.join(RESULT_DATA_PATH, "wrong_seq.jsonl"), "a", encoding="utf-8") as wrong_file:
+                    #     data = {"uc": uc_data, "temperature": temp, "top_p": top_p, "content": content}
+                    #     print(json.dumps(data), file=wrong_file)
+                    save_error(os.path.join(RESULT_DATA_PATH, "wrong_seq.jsonl"), uc_data, temp, top_p, "Empty response", content)
                     continue
                 
                 seq_data = plantumlfiles.process_diagram(seq_diag.group(1), Origin.CHAT_GPT, "", None, context.ai_model, temp, top_p)
                     
-                if seq_data is not None:
-                    print(json.dumps(seq_data), file=seq_file)
-                    print(json.dumps(merge_metadata(uc_data, seq_data)), file=output_file)
+                if seq_data is None:
+                    save_error(os.path.join(RESULT_DATA_PATH, "wrong_seq.jsonl"), uc_data, temp, top_p, "Wrong diagram", content)
+                    continue
+                    
+                name = uc_data["title"]
+                print(json.dumps(seq_data), file=seq_file)
+                print(json.dumps(merge_metadata(uc_data, seq_data, name=name)), file=output_file)
                 
-            print(time.perf_counter() - start, f"price: {price}$")
-            break
         
     output_file.close()
     seq_file.close()
+    
+    total = time.perf_counter() - start
+    print(f"Total time: {total}", f"Average time: {total / counter}", f"price: {price}$")
+    
+uc_seq()
 
 # Using temperature
 # with open(os.path.join(WORKING_DIR, "data\\usecases\\01.xml"), "r", encoding="utf-8") as file:
@@ -223,34 +240,55 @@ def uc_seq():
 
 
 ############### PROMPT ###############
-example_diagam = """@startuml
-autoactivate on
-hide footbox
+# example_diagam = """@startuml
+# autoactivate on
+# hide footbox
 
-actor User
-participant System
-participant "Database" as DB
+# actor User
+# participant System
+# participant "Database" as DB
 
-User -> System : Request Data
-System -> DB : Query Data
-alt Data Found
-    return Return Data
-    loop Process Data
-        System -> System : Process Data
-    end
-    System --> User : Display Data
-else Data Not Found
-    activate DB
-    System <-- DB : Return Error
-    User <-- System : Display Error
-end
-@enduml"""
+# User -> System : Request Data
+# System -> DB : Query Data
+# alt Data Found
+#     return Return Data
+#     loop Process Data
+#         System -> System : Process Data
+#     end
+#     System --> User : Display Data
+# else Data Not Found
+#     activate DB
+#     System <-- DB : Return Error
+#     User <-- System : Display Error
+# end
+# @enduml"""
 
 # with open(os.path.join(WORKING_DIR, "data\\usecases\\01.xml"), "r") as file:
 #     example_use_case = file.read()
 
 # with open(DTD_SCHEME_PATH, "r") as file:
 #     dtd = file.read()
+    
+# promt_ctx = [
+#                 {"role": "developer", "content": "Based on the following use case, create a sequence diagram in plantuml without any comments."},
+#                 {"role": "assistant", "content": "Noted."},
+#                 {"role": "user", "content": "In sequence diagram if name of element is multiple words, put it in quotation marks and give diagram title"},
+#                 {"role": "assistant", "content": "OK"},
+#                 {"role": "user", "content": "Analyze the given software engineering diagram and classify it into an appropriate application domain like Banking, Education, Healthcare, Finance, etc. based on its context. Add only domain after diagram as last line of response inside {{}}"},
+#                 ]
+
+# query = f"Here is use case {example_use_case}"
+            
+# result = context.execute_query(query, promt_ctx, 1, 0.8)[0]
+# content = result["choices"][0]["message"]["content"]
+
+# price = calculate_price(result["usage"]["prompt_tokens"], result["usage"]["completion_tokens"])
+
+# domain = re.search(r"\{\{(.*)\}\}", content).group(1)
+# seq_diag = re.search(r"(@startuml.*@enduml)", content, re.DOTALL)
+
+# print(content)
+# print(price, domain, "\n", seq_diag)
 
 # promt_ctx = [{"role": "user", "content": f"Here is example of sequence diagram: {example_diagam}"},
 #              {"role": "user", "content": f"Here is dtd scheme for usecase: {dtd}"},
